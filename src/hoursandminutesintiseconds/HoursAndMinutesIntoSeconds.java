fcbb37ca4b72e2d1d358df91082b0b0f64430fb7
PUJARI RAJESH <rajesh.pujari@improve10x.com>
2023-07-20 23:19:11 +0530

    I wrote if condition

--- src/hoursandminutesintiseconds/HoursAndMinutesIntoSeconds.java
+++ src/hoursandminutesintiseconds/HoursAndMinutesIntoSeconds.java
@@ -4,11 +4,13 @@ public class HoursAndMinutesIntoSeconds {
     public int convertHrsAndMinsIntoSeconds(int hours, int minutes) {
         int hrsSeconds = 0;
         int minsSeconds = 0;
-        if (hours > 0) {
-            hrsSeconds = hours * 60 * 60;
-        }
-        if (minutes > 0) {
-            minsSeconds = minutes * 60;
+        if (hours > 0 || minutes > 0) {
+            if (hours > 0) {
+                hrsSeconds = hours * 60 * 60;
+            }
+            if (minutes > 0) {
+                minsSeconds = minutes * 60;
+            }
         }
         return hrsSeconds + minsSeconds;
     }
