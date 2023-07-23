299ea270062aab9137fd7b1b602d7ec394aee427
PUJARI RAJESH <rajesh.pujari@improve10x.com>
2023-07-23 23:08:43 +0530

    I created templerun package, PlayerTest.java, Player.java, CoinTest.java, Coin.java and I wrote testcases and functionality

---
+++ src/templerun/Player.java
@@ -0,0 +1,50 @@
+package templerun;
+
+public class Player {
+
+    private String name;
+    private int health;
+    private int totalCoinValue;
+    private int score;
+
+
+    public Player(String name) {
+        this(name, 100);
+    }
+
+    public Player(String name, int health) {
+        this.name = name == null ? "" : name.trim();
+        if (health < 0 || health > 100) {
+            throw new InvalidHealthException();
+        }
+        this.health = health;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public int getScore() {
+        score = totalCoinValue;
+        return score;
+    }
+
+    public void run() {
+        System.out.println("Running...!");
+    }
+
+    public void collectCoin(Coin coin) {
+        this.totalCoinValue += coin.getValue();
+    }
+
+    public void jump() {
+        System.out.println(name + " jumped to avoid obstacles!");
+    }
+
+    public class InvalidHealthException extends RuntimeException {
+    }
+}
