48af471c9f999c11c36d8142f0c0b8d1f1d37eb5
PUJARI RAJESH <rajesh.pujari@improve10x.com>
2023-08-05 16:41:00 +0530

    I wrote nullValue, zeroValue testcase.

--- src/findlargestnumber/FindLargestNumberTestCase.java
+++ src/findlargestnumber/FindLargestNumberTestCase.java
@@ -1,15 +1,34 @@
 package findlargestnumber;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FindLargestNumberTestCase {
 
+    private FindLargestNumber findLargestNumber;
+
+    @BeforeEach
+    public void setup() {
+        findLargestNumber = new FindLargestNumber();
+    }
+
     @Test
     public void givenEmpty_returnsZero() {
-        FindLargestNumber findLargestNumber = new FindLargestNumber();
         int empty = findLargestNumber.findLargestElement(new int[] {});
         assertEquals(0, empty);
+    }
+
+    @Test
+    public void givenNull_returnsZero() {
+        int nullValue = findLargestNumber.findLargestElement(null);
+        assertEquals(0, nullValue);
+    }
+
+    @Test
+    public void givenZero_returnsZero() {
+        int zeroValue = findLargestNumber.findLargestElement(new int[] {0});
+        assertEquals(0, zeroValue);
     }
 }
