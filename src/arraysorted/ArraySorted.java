350e170345bb000f7692798ac46387d32e63a1f9
PUJARI RAJESH <rajesh.pujari@improve10x.com>
2023-08-07 17:49:28 +0530

    I implemented functionality, some testcases and code refactor

--- src/arraysorted/ArraySorted.java
+++ src/arraysorted/ArraySorted.java
@@ -2,6 +2,21 @@ package arraysorted;
 
 public class ArraySorted {
     public boolean isArraySorted(int[] arr) {
+        if (arr == null || arr.length == 0) {
+            return false;
+        } else if (arr.length == 1) {
+            return true;
+        }
+        if (isSortedOrNot(arr)) return false;
+        return true;
+    }
+
+    private static boolean isSortedOrNot(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < arr[i-1]) {
+                return true;
+            }
+        }
         return false;
     }
 }
--- src/arraysorted/ArraySortedTestCase.java
+++ src/arraysorted/ArraySortedTestCase.java
@@ -1,22 +1,52 @@
 package arraysorted;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ArraySortedTestCase {
 
+    private ArraySorted arraySorted;
+
+    @BeforeEach
+    private void setup() {
+        arraySorted = new ArraySorted();
+    }
+
     @Test
     public void givenNull_returnsFalse() {
-        ArraySorted arraySorted = new ArraySorted();
         boolean nullValue = arraySorted.isArraySorted(null);
         assertEquals(false, nullValue);
     }
 
     @Test
     public void givenEmpty_returnsFalse() {
-        ArraySorted arraySorted = new ArraySorted();
         boolean emptyValue = arraySorted.isArraySorted(new int[] {});
         assertEquals(false, emptyValue);
+    }
+
+    @Test
+    public void givenZero_returnsTrue() {
+        boolean zeroValue = arraySorted.isArraySorted(new int[] {0});
+        assertEquals(true, zeroValue);
+    }
+
+    @Test
+    public void givenOne_returnsTrue() {
+        boolean oneValue = arraySorted.isArraySorted(new int[] {1});
+        assertEquals(true, oneValue);
+    }
+
+    @Test
+    public void givenZeroAndOne_returnsTrue() {
+        boolean zeroAndOneValue = arraySorted.isArraySorted(new int[] {-5,-4,-3,-2});
+        assertEquals(true, zeroAndOneValue);
+    }
+
+    @Test
+    public void givenOneAndZero_returnsFalse() {
+        boolean zeroAndOneValue = arraySorted.isArraySorted(new int[] {-0, -1});
+        assertEquals(false, zeroAndOneValue);
     }
 }
