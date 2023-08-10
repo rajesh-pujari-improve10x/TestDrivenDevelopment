a167c3595166cef1a19482a2b96b19db8c57cf2b
PUJARI RAJESH <rajesh.pujari@improve10x.com>
2023-08-10 13:20:36 +0530

    i did code refactor in LargestOddNumberTestCase.java class.

--- src/largestoddnumber/LargestOddNumberTestCase.java
+++ src/largestoddnumber/LargestOddNumberTestCase.java
@@ -1,66 +1,66 @@
 package largestoddnumber;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class LargestOddNumberTestCase {
 
+    private LargestOddNumber largestOddNumber;
+
+    @BeforeEach
+    public void setup() {
+        largestOddNumber = new LargestOddNumber();
+    }
+
     @Test
     public void nothing() {}
 
     @Test
     public void givenNull_returnEmpty() {
-        LargestOddNumber largestOddNumber = new LargestOddNumber();
         String nullValue = largestOddNumber.largestOddNumberInStr(null);
         assertEquals("", nullValue);
     }
 
     @Test
     public void givenEmpty_returnEmpty() {
-        LargestOddNumber largestOddNumber = new LargestOddNumber();
         String emptyValue = largestOddNumber.largestOddNumberInStr("");
         assertEquals("", emptyValue);
     }
 
     @Test
     public void givenZeroValue_returnEmpty() {
-        LargestOddNumber largestOddNumber = new LargestOddNumber();
         String zeroValue = largestOddNumber.largestOddNumberInStr("0");
         assertEquals("", zeroValue);
     }
 
     @Test
     public void givenOneValue_returnOne() {
-        LargestOddNumber largestOddNumber = new LargestOddNumber();
         String oneValue = largestOddNumber.largestOddNumberInStr("1");
         assertEquals("1", oneValue);
     }
 
     @Test
     public void givenTwoValue_returnEmpty() {
-        LargestOddNumber largestOddNumber = new LargestOddNumber();
         String twoValue = largestOddNumber.largestOddNumberInStr("2");
         assertEquals("", twoValue);
     }
 
     @Test
     public void givenFiveTwo_returnFive() {
-        LargestOddNumber largestOddNumber = new LargestOddNumber();
         String fiveTwoValue = largestOddNumber.largestOddNumberInStr("52");
         assertEquals("5", fiveTwoValue);
     }
 
     @Test
     public void given4206_returnEmpty() {
-        LargestOddNumber largestOddNumber = new LargestOddNumber();
         String result = largestOddNumber.largestOddNumberInStr("4206");
         assertEquals("", result);
     }
 
     @Test
     public void given35427_return35427() {
-        LargestOddNumber largestOddNumber = new LargestOddNumber();
         String result = largestOddNumber.largestOddNumberInStr("35427");
         assertEquals("35427", result);
     }
