e88c4f25f9c5ac36be6e3e0bb7ae5466201da29f
PUJARI RAJESH <rajesh.pujari@improve10x.com>
2023-08-10 15:53:20 +0530

    I wrote givenNull_returnMinusOne(), givenEmpty_returnMinusOne() testcases and created MajorityElement.java class.

---
+++ src/majorityelement/MajorityElement.java
@@ -0,0 +1,7 @@
+package majorityelement;
+
+public class MajorityElement {
+    public int findMajorityElement(int[] arr) {
+        return -1;
+    }
+}
--- src/majorityelement/MajorityElementTestCase.java
+++ src/majorityelement/MajorityElementTestCase.java
@@ -2,8 +2,24 @@ package majorityelement;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MajorityElementTestCase {
 
     @Test
     public void nothing() {}
+
+    @Test
+    public void givenNull_returnMinusOne() {
+        MajorityElement majorityElement = new MajorityElement();
+        int majorityValue = majorityElement.findMajorityElement(null);
+        assertEquals(-1, majorityValue);
+    }
+
+    @Test
+    public void givenEmpty_returnMinusOne() {
+        MajorityElement majorityElement = new MajorityElement();
+        int majorityValue = majorityElement.findMajorityElement(new int[] {});
+        assertEquals(-1, majorityValue);
+    }
 }
