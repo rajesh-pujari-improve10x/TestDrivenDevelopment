3e1aa5984ade1e5e9a15e21e34fbd9d27f940c87
PUJARI RAJESH <rajesh.pujari@improve10x.com>
2023-08-10 14:44:55 +0530

    code refactor in LargestOddNumber.java class.

--- src/largestoddnumber/LargestOddNumber.java
+++ src/largestoddnumber/LargestOddNumber.java
@@ -2,13 +2,12 @@ package largestoddnumber;
 
 public class LargestOddNumber {
     public String largestOddNumberInStr(String number) {
-        if (number == null || number.equals("") || number.equals("0")) {
-            return "";
-        }
-        String num = number.trim();
-        for (int i = num.length() - 1; i >= 0; i--) {
-            if (num.charAt(i)%2 != 0) {
-                return num.substring(0, i + 1);
+        if (number != null && !number.equals("")) {
+            String num = number.trim();
+            for (int i = num.length() - 1; i >= 0; i--) {
+                if (num.charAt(i)%2 != 0) {
+                    return num.substring(0, i + 1);
+                }
             }
         }
         return "";
