472070e15821d59002d5fb8d44ba0f9c8ea359f3
PUJARI RAJESH <rajesh.pujari@improve10x.com>
2023-08-11 16:50:19 +0530

    I had code refactor in ReverseWord.java class.

--- src/reverseword/ReverseWord.java
+++ src/reverseword/ReverseWord.java
@@ -3,13 +3,12 @@ package reverseword;
 public class ReverseWord {
     public String reverseWordsInAString(String str) {
         String reverseWord = "";
-        if (str != null && !str.equals("")) {
+        if (str != null && !str.isEmpty()) {
             String[] words = str.trim().split("\\s+");
             for (int i = words.length - 1; i >= 0; i--) {
                 reverseWord += words[i] + " ";
             }
-            return reverseWord.trim();
         }
-        return "";
+        return reverseWord.trim();
     }
 }
