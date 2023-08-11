cff0303b37b9e9384a12985314943a0f961727d8
PUJARI RAJESH <rajesh.pujari@improve10x.com>
2023-08-11 16:46:57 +0530

    I wrote givenThisIsAnSpacesAmazingProgram_returnProgramAmazingAnIsThis() testcase and code refactor in ReverseWordTestCase.java class.

--- src/reverseword/ReverseWord.java
+++ src/reverseword/ReverseWord.java
@@ -4,7 +4,7 @@ public class ReverseWord {
     public String reverseWordsInAString(String str) {
         String reverseWord = "";
         if (str != null && !str.equals("")) {
-            String[] words = str.trim().split(" ");
+            String[] words = str.trim().split("\\s+");
             for (int i = words.length - 1; i >= 0; i--) {
                 reverseWord += words[i] + " ";
             }
--- src/reverseword/ReverseWordTestCase.java
+++ src/reverseword/ReverseWordTestCase.java
@@ -6,69 +6,68 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ReverseWordTestCase {
 
+    private final ReverseWord reverseWord = new ReverseWord();
+
     @Test
     public void nothing() {}
 
     @Test
     public void givenNull_returnEmpty() {
-        ReverseWord reverseWord = new ReverseWord();
         String strReverseWords = reverseWord.reverseWordsInAString(null);
         assertEquals("", strReverseWords);
     }
 
     @Test
     public void givenEmpty_returnEmpty() {
-        ReverseWord reverseWord = new ReverseWord();
         String strReverseWords = reverseWord.reverseWordsInAString("");
         assertEquals("", strReverseWords);
     }
 
     @Test
     public void givenRChar_returnR() {
-        ReverseWord reverseWord = new ReverseWord();
         String strReverseWords = reverseWord.reverseWordsInAString("R");
         assertEquals("R", strReverseWords);
     }
 
     @Test
     public void givenRCharWithSpaces_returnR() {
-        ReverseWord reverseWord = new ReverseWord();
         String strReverseWords = reverseWord.reverseWordsInAString("   R  ");
         assertEquals("R", strReverseWords);
     }
 
     @Test
     public void givenRajWithSpacesLeftSide_returnRaj() {
-        ReverseWord reverseWord = new ReverseWord();
         String strReverseWords = reverseWord.reverseWordsInAString("   Raj");
         assertEquals("Raj", strReverseWords);
     }
 
     @Test
     public void givenRajWithSpacesRightSide_returnRaj() {
-        ReverseWord reverseWord = new ReverseWord();
         String strReverseWords = reverseWord.reverseWordsInAString("Raj    ");
         assertEquals("Raj", strReverseWords);
     }
 
     @Test
     public void givenRajesh_returnRajesh() {
-        ReverseWord reverseWord = new ReverseWord();
         String strReverseWords = reverseWord.reverseWordsInAString("Rajesh");
         assertEquals("Rajesh", strReverseWords);
     }
 
     @Test
     public void givenRajeshSpacePujari_returnPujariSpaceRajesh() {
-        ReverseWord reverseWord = new ReverseWord();
         String strReverseWords = reverseWord.reverseWordsInAString("Rajesh Pujari");
         assertEquals("Pujari Rajesh", strReverseWords);
     }
 
     @Test
     public void givenThisIsAnAmazingProgram_returnProgramAmazingAnIsThis() {
-        ReverseWord reverseWord = new ReverseWord();
         String strReverseWords = reverseWord.reverseWordsInAString("this is an amazing program");
+        assertEquals("program amazing an is this", strReverseWords);
+    }
+
+    @Test
+    public void givenThisIsAnSpacesAmazingProgram_returnProgramAmazingAnIsThis() {
+        String strReverseWords = reverseWord.reverseWordsInAString("this is an      amazing program");
         assertEquals("program amazing an is this", strReverseWords);
     }
 }
