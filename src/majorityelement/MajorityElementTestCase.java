07795226bff020a79e98fa4f741f495d289f7f41
PUJARI RAJESH <rajesh.pujari@improve10x.com>
2023-08-11 10:55:28 +0530

    I wrote givenTwoOnes_returnOne() testcase.

--- src/majorityelement/MajorityElementTestCase.java
+++ src/majorityelement/MajorityElementTestCase.java
@@ -50,4 +50,11 @@ public class MajorityElementTestCase {
         int majorityValue = majorityElement.findMajorityElement(new int[] {0, 1});
         assertEquals(-1, majorityValue);
     }
+
+    @Test
+    public void givenTwoOnes_returnOne() {
+        MajorityElement majorityElement = new MajorityElement();
+        int majorityValue = majorityElement.findMajorityElement(new int[] {1, 1});
+        assertEquals(1, majorityValue);
+    }
 }
